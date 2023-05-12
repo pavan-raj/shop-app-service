18340f83e23dd3f6e4bbb8a6aa8c84f71a60f953
pavan-raj <chavanpavanraj@gmail.com>
2023-05-12 15:44:44 +0530

    removed id from customer request

--- src/main/java/com/fin/shop/api/CustomerRequest.java
+++ src/main/java/com/fin/shop/api/CustomerRequest.java
@@ -4,18 +4,10 @@ import java.util.Date;
 
 public class CustomerRequest {
 
-    private Long id;
     private String firstName;
     private String lastName;
     private String address;
     private String phoneNumber;
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
